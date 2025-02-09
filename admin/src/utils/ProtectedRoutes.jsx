ed25e983f2891a866a11b0564c7b0a20096cc672
Avinash <avinashviolin4@gmail.com>
2025-02-09 23:37:41 +0530

    fixed ProtectedRoutes.jsx and removed async

--- admin/src/utils/ProtectedRoutes.jsx
+++ admin/src/utils/ProtectedRoutes.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { Outlet, Navigate } from "react-router-dom";
 
-async function ProtectedRoutes() {
-  const response = await axios.get("http://localhost:3000", {
+function ProtectedRoutes() {
+  const response = axios.get("http://localhost:3000", {
     withCredentials: true,
   });
   return response ? <Outlet /> : <Navigate to={"/"} />;
